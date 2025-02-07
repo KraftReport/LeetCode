63f2f6824164dea2d01a8e1a298969b335297517
Myo Set Paing <kraftreport04@gmail.com>
2025-02-07 22:41:09 +0630

    leetcode

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import solutions.MergeAlternativelySolution;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println(MergeAlternativelySolution.mergeAlternately("hlowrd","el ol"));
+	}
+}
---
+++ src/solutions/MergeAlternativelySolution.java
@@ -0,0 +1,36 @@
+package solutions;
+
+public class MergeAlternativelySolution {
+
+    public static String mergeAlternately(String word1, String word2) {
+    	var result = "";
+        var firstWordSize = word1.length();
+        var secondWordSize = word2.length();
+        
+        var commonLength = firstWordSize >= secondWordSize ? firstWordSize : secondWordSize;
+        
+        for(int i = 0 ; i < commonLength ; i++) {
+        	
+        	var boxOne = "";
+        	var boxTwo = "";
+        	
+        	try {
+        		boxOne = ""+word1.charAt(i);
+        	}catch(Exception ex) {
+        		boxOne = "";
+        	}
+        	
+        	try {
+        		boxTwo = ""+word2.charAt(i);
+        	}catch(Exception ex) {
+        		boxTwo = "";
+        	} 
+        	
+        	result += boxOne+boxTwo;
+        }
+        
+        return result;
+        
+    }
+
+}

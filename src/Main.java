97d17568ae8650524a92f88cc7fcba553a4c536a
Myo Set Paing <kraftreport04@gmail.com>
2025-02-11 22:29:49 +0630

    leetcode

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 import solutions.GreatestCommonDivisorOfStrings;
+import solutions.KidsWithTheGreatestNumberOfCandies;
 import solutions.MergeAlternatively;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println(MergeAlternatively.solution("hlowrd","el ol"));
-		System.out.println(GreatestCommonDivisorOfStrings.solution("helloworld", "hello"));
+//		System.out.println(MergeAlternatively.solution("hlowrd","el ol"));
+//		System.out.println(GreatestCommonDivisorOfStrings.solution("helloworld", "hello"));
+		System.out.println(KidsWithTheGreatestNumberOfCandies.solution(new int[] {2,3,5,1,3}, 3));
 	}
 }

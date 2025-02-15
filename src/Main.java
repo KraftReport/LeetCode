9a96d68c17e4b52bd39717bc29ecd03b3b508725
Myo Set Paing <kraftreport04@gmail.com>
2025-02-15 23:32:34 +0630

    leetcode

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import solutions.GreatestCommonDivisorOfStrings;
 import solutions.KidsWithTheGreatestNumberOfCandies;
 import solutions.MergeAlternatively;
 import solutions.ReverseVowelsOfAString;
+import solutions.ReverseWordsInAString;
 
 public class Main {
 
@@ -11,6 +12,9 @@ public class Main {
 //		System.out.println(GreatestCommonDivisorOfStrings.solution("helloworld", "hello"));
 //		System.out.println(KidsWithTheGreatestNumberOfCandies.solution(new int[] {2,3,5,1,3}, 3));
 //		System.out.println(CanPlaceFolwers.solution(new int[] {0,0,0,0,1}, 2));
-		System.out.println(ReverseVowelsOfAString.solution("leetcode"));
+//		System.out.println(ReverseVowelsOfAString.solution("leetcode"));
+		System.out.println(ReverseWordsInAString.solution("the sky is blue"));
+		System.out.println(ReverseWordsInAString.solution("a good   example"));
+		System.out.println(ReverseWordsInAString.solution("  hello world  "));
 	}
 }

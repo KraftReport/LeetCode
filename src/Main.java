d57469bccf9d7cda92b527cb3c842f060a893282
Myo Set Paing <kraftreport04@gmail.com>
2025-02-19 22:43:11 +0630

    leetcode

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ import solutions.MergeAlternatively;
 import solutions.ProductOfTheArrayExceptSelf;
 import solutions.ReverseVowelsOfAString;
 import solutions.ReverseWordsInAString;
+import solutions.StringCompression;
 
 public class Main {
 
@@ -19,8 +20,9 @@ public class Main {
 //		System.out.println(ReverseVowelsOfAString.solution("leetcode")); 
 //		System.out.println(ReverseWordsInAString.solution("  hello world  "));
 //		Arrays.stream(ProductOfTheArrayExceptSelf.solution(new int[] {-1,1,0,-3,3})).boxed().toList().forEach(System.out::println);
-		System.out.println(IncreasingTripletSubsequence.solution(new int[] {1,2,3,4,5}));
-		System.out.println(IncreasingTripletSubsequence.solution(new int[] {1,9,0,10}));
-		System.out.println(IncreasingTripletSubsequence.solution(new int[] {1,5,0,4,1,3}));
+//		System.out.println(IncreasingTripletSubsequence.solution(new int[] {1,2,3,4,5}));
+//		System.out.println(IncreasingTripletSubsequence.solution(new int[] {1,9,0,10}));
+//		System.out.println(IncreasingTripletSubsequence.solution(new int[] {1,5,0,4,1,3}));
+		System.out.println(StringCompression.solution(new char[] {'a','a','b','b','c','c','c'}));
 	}
 }

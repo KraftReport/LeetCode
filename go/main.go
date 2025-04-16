04120c31a81347bbdb4b23e6fe27f762f8066e3e
Myo Set Paing <kraftreport04@gmail.com>
2025-04-16 14:39:07 +0630

    teemo attacks ashe with annoying poision darts

--- go/main.go
+++ go/main.go
@@ -10,5 +10,6 @@ func main() {
 	// fmt.Println(solutions.MissingNumberSolution([]int{1,0,3}))
 	// fmt.Println(solutions.AssignCookieSolution([]int{1,2,3},[]int{1,1}))
 	// fmt.Println(solutions.IslandPerimeterSolution([][]int{{0,1,0,0},{1,1,1,0},{0,1,0,0},{1,1,0,0}}))
-	fmt.Println(solutions.MaxConsecutiveOnesSolution([]int{1,1,0,1,1,1}))
+	// fmt.Println(solutions.MaxConsecutiveOnesSolution([]int{1,1,0,1,1,1}))
+	fmt.Println(solutions.TeemoAttackSolution([]int{1,4},2))
 }

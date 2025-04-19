094a9403791ffaadd7f9bb9c347008fdf1c275ff
Myo Set Paing <kraftreport04@gmail.com>
2025-04-20 00:38:50 +0630

    valid parenthese solution

--- go/main.go
+++ go/main.go
@@ -13,5 +13,6 @@ func main() {
 	// fmt.Println(solutions.MaxConsecutiveOnesSolution([]int{1,1,0,1,1,1}))
 	// fmt.Println(solutions.TeemoAttackSolution([]int{1,4},2))
 	// fmt.Println(solutions.NextGreaterElement([]int{4,1,2},[]int{1,3,4,2}))
-	fmt.Println(solutions.KeyboardRowSolution([]string{"Hello","Alaska","Dad","Peace"}))
+	// fmt.Println(solutions.KeyboardRowSolution([]string{"Hello","Alaska","Dad","Peace"}))
+	fmt.Println(solutions.ValidParenthesesSolution("()"))
 }

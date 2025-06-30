5b36194265edf1335c9328994af4e0f8231c8e0b
Myo Set Paing <138693417+KraftReport@users.noreply.github.com>
2025-06-30 16:52:17 +0630

    start up again

---
+++ c#/Console/Program.cs
@@ -0,0 +1,5 @@
+﻿using Console.Solutions;
+using System;
+
+
+System.Console.WriteLine(LongestHarmoniousSubsequence.Solution(new int[] { 1, 3, 2, 2, 5, 2, 3, 7 }));
---
+++ c#/Console/Solutions/LongestHarmoniousSubsequence.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console.Solutions
+{
+    public class LongestHarmoniousSubsequence
+    {
+        public static int Solution(int[] nums)
+        {
+            var longestValue = 0; 
+
+            for (var i = 0; i < nums.Length-1; i++)
+            {
+                if (nums[i+1] - nums[i] < 2)
+                {
+                    longestValue++;
+                } 
+            }
+
+            return longestValue;
+        }
+    }
+}

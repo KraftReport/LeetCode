7a110a35f07cdfe22e35163f51305c34907d7f58
Myo Set Paing <138693417+KraftReport@users.noreply.github.com>
2025-07-18 22:29:01 +0630

    jewels and stones

---
+++ c#/Console/Solutions/JewelsAndStones.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console.Solutions
+{
+    public class JewelsAndStones
+    {
+        public static int Solution(string jewels,string stones)
+        {
+            int myJewels = 0;
+            
+            foreach(char stone in stones)
+            {
+                if (jewels.Contains(stone))
+                {
+                    myJewels++;
+                }
+            }
+            
+            return myJewels;
+        }
+    }
+}

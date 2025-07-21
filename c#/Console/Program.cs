827d8abb904903edb28676d58b6851450ced0311
Myo Set Paing <138693417+KraftReport@users.noreply.github.com>
2025-07-21 21:36:20 +0630

    defanged ip address

--- c#/Console/Program.cs
+++ c#/Console/Program.cs
@@ -28,4 +28,6 @@ using System;
 
 //System.Console.WriteLine(UniqueMossCodeWords.Solution(new string[] { "gin", "zen", "gig", "msg" }));
 
-System.Console.WriteLine(NumberOfUniqueEmails.Solution(new string[] { "test.email+alex@leetcode.com", "test.e.mail+bob.cathy@leetcode.com", "testemail+david@lee.tcode.com" }));
+//System.Console.WriteLine(NumberOfUniqueEmails.Solution(new string[] { "test.email+alex@leetcode.com", "test.e.mail+bob.cathy@leetcode.com", "testemail+david@lee.tcode.com" }));
+
+System.Console.WriteLine(DefangingIpAddress.Solution("1.1.1.1"));

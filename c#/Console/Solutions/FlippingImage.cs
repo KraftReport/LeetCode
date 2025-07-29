fb00fd9e5856591be3ca8c22930c8b364699dce4
Myo Set Paing <138693417+KraftReport@users.noreply.github.com>
2025-07-29 23:24:56 +0630

    flipping image

---
+++ c#/Console/Solutions/FlippingImage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console.Solutions
+{
+    public static class FlippingImage
+    {
+        public static int[][] Solution(int[][] image)
+        {  
+            for(int i = 0; i < image.Length; i++)
+            {
+                int left = 0;
+                
+                int right = image[i].Length-1;
+
+                while (left <= right)
+                {
+                    if(left == right)
+                    {
+                        image[i][left] ^= 1;
+                    }
+                    else
+                    {
+                        int temp = image[i][left] ^ 1;
+                        
+                        image[i][left] = image[i][right] ^ 1;
+                        
+                        image[i][right] = temp;
+                    }
+                    left++;
+                    right--;
+                }
+            }
+            return image;
+        }
+    }
+}
---
+++ c#/Console/Solutions/HappyNumber.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console.Solutions
+{
+    public class HappyNumber
+    {
+        public static bool Solution(int n)
+        {
+            
+            int slow = n;
+            
+            int fast = Sum(n);
+
+            while(fast != 1 && slow != fast)
+            {
+                
+                slow = Sum(slow);
+                
+                fast = Sum(Sum(fast));
+            }
+
+            return fast == 1;
+        }
+
+        private static int Sum(int n)
+        {
+            int sum = 0;
+            
+            while(n> 0)
+            {
+                int digit = n % 10;
+            
+                sum += digit * digit;
+                
+                n /= 10;
+            }
+            
+            return sum;
+        }
+    }
+}

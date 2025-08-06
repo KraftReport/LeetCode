1ba5191991ddcfac468c1a6cc4e6e62e33fe33a1
Myo Set Paing <138693417+KraftReport@users.noreply.github.com>
2025-08-07 01:10:26 +0630

    reverse strings

--- c#/Console/Program.cs
+++ c#/Console/Program.cs
@@ -50,11 +50,13 @@ using System;
 
 //System.Console.WriteLine(FirstPlanedrome.Solution(new string[] { "abc", "car", "ada", "racecar", "cool" }));
 
-System.Console.WriteLine(MergeTwoDimensionalArraysBySummingValues.Solution(new int[][] {
-    new int[] { 2, 4 },
-    new int[] { 3, 6 },
-    new int[] { 5, 5 }
-}, new int[][] {
-    new int[] { 1, 3 },
-    new int[] { 4, 3 }
-}));
+//System.Console.WriteLine(MergeTwoDimensionalArraysBySummingValues.Solution(new int[][] {
+//    new int[] { 2, 4 },
+//    new int[] { 3, 6 },
+//    new int[] { 5, 5 }
+//}, new int[][] {
+//    new int[] { 1, 3 },
+//    new int[] { 4, 3 }
+//}));
+
+System.Console.WriteLine(ReverseString.Solution("Let's take LeetCode contest"));

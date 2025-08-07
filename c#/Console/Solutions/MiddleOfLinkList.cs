678fe694681953f7bc769abf9470bb310e296f58
Myo Set Paing <138693417+KraftReport@users.noreply.github.com>
2025-08-07 22:44:54 +0630

    mddle of link lists

---
+++ c#/Console/Solutions/MiddleOfLinkList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console.Solutions
+{    
+    public static class MiddleOfLinkList
+    {
+        public static ListNode Solution(ListNode head)
+        {
+            ListNode slow = head;
+            ListNode fast = head;
+
+            while(fast != null && fast.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+
+            return slow;
+        }
+    }
+}
+
+ 

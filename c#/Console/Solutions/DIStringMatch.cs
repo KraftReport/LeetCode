1cd2c2514515bca7d9c321c9150a7809fce1ab90
Myo Set Paing <138693417+KraftReport@users.noreply.github.com>
2025-08-08 23:48:54 +0630

    di string match

---
+++ c#/Console/Solutions/DIStringMatch.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console.Solutions
+{
+    public static class DIStringMatch
+    {
+        public static int[] Solution(string s)
+        {
+            int[] result = new int[s.Length + 1];
+            int low = 0;
+            int high = s.Length;
+            int index = 0;
+            
+            while(low <= high)
+            {
+                if(low == high)
+                {
+                    result[index] = low;
+                    break;
+                }
+
+                if (s[index] == 'I')
+                {
+                    result[index] = low;
+                    low++;
+                    index++;
+                    continue;
+                }
+
+                if (s[index] == 'D')
+                {
+                    result[index] = high;
+                    high--;
+                    index++;
+                    continue;
+                }
+            }  
+            return result;
+        }
+    }
+}

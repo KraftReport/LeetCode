8be801f3073b2be542ca736d452d34d3bfbc4e62
Myo Set Paing <138693417+KraftReport@users.noreply.github.com>
2025-08-26 23:11:13 +0630

    convert an array into 2d array with conditions

---
+++ c#/Console/Solutions/ConvertAnArrayInto2DArrayWithConditions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console.Solutions
+{
+    public static class ConvertAnArrayInto2DArrayWithConditions
+    {
+        public static IList<IList<int>> Solution(int[] nums)
+        {
+            List<int> list = new List<int>(nums);
+            IList<IList<int>> result = new List<IList<int>>();
+
+            while (list.Count != 0)
+            {
+                List<int> subList = new List<int>();
+                HashSet<int> set = new HashSet<int>(); 
+                int skip = 0;
+                while (skip < list.Count)
+                {
+                    if (!set.Contains(list[skip]))
+                    {
+                        subList.Add(list[skip]);
+                        set.Add(list[skip]);
+                        list.RemoveAt(skip);
+                    }
+                    else
+                    {
+                        skip++;
+                    }
+                }
+                result.Add(subList);
+            } 
+            return result;
+        }
+    }
+}

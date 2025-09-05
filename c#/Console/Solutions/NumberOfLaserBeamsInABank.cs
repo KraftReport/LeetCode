04d8957cfa61efd6b993a867e8f88e5d9ba7a89b
Myo Set Paing <138693417+KraftReport@users.noreply.github.com>
2025-09-05 22:54:03 +0630

    number of laser beams in a bank

---
+++ c#/Console/Solutions/NumberOfLaserBeamsInABank.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console.Solutions
+{
+    public static class NumberOfLaserBeamsInABank
+    {
+        public static int Solution(string[] bank)
+        {
+            int laserBeams = 0; 
+            for (int i = 0; i < bank.Length; i++)
+            {
+                char[] floor = bank[i].ToCharArray();
+                foreach(char securityDevice in floor)
+                {
+                    bool isSecurityDevicePresentBetweenFloors = false;
+                    if (securityDevice == '1')
+                    { 
+                        for (int j = i + 1; j < bank.Length; j++)
+                        {
+                            if(!isSecurityDevicePresentBetweenFloors)
+                            {
+                                char[] upperFloor = bank[j].ToCharArray();
+                                foreach (char upperFloorSecurityDevice in upperFloor)
+                                {
+                                    if (upperFloorSecurityDevice == '1')
+                                    {
+                                        laserBeams++; 
+                                        isSecurityDevicePresentBetweenFloors = true;
+                                    }
+                                }
+                            } 
+                        }
+                    }
+                }
+            }
+            return laserBeams;
+        }
+    }
+}

695a0d679cc5a2dc8b344870fcbd3a411dc47741
Myo Set Paing <138693417+KraftReport@users.noreply.github.com>
2025-09-07 00:34:37 +0630

    Create MinimunNumberOfOperationsToMakeArrayXOREqualToK.cs

---
+++ c#/Console/Solutions/MinimunNumberOfOperationsToMakeArrayXOREqualToK.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console.Solutions
+{
+    public static class MinimunNumberOfOperationsToMakeArrayXOREqualToK
+    {
+        public static int Solution(int[] nums, int k)
+        {
+            int count = 0;
+            int xor = 0;
+            foreach (int num in nums) xor ^= num;
+            int diff = xor ^ k;
+            while(diff > 0)
+            {
+                count += (diff & 1);
+                diff >>= 1;
+            }
+            return count;
+        }
+    }
+}

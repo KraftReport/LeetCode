910f8ff89dbd23d8e35ce7ab454657416391d992
Myo Set Paing <138693417+KraftReport@users.noreply.github.com>
2025-09-13 23:36:12 +0630

    maximun number of coins you can get

---
+++ c#/Console/Solutions/MaximunNumberOfCoinYouCanGet.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console.Solutions
+{
+    public static class MaximunNumberOfCoinYouCanGet
+    {
+        public static int Solution(int[] piles)
+        {
+            int result = 0; 
+            List<int> ascSortedList = piles.OrderBy(x =>x).ToList(); 
+            while(ascSortedList.Count > 0)
+            {
+                ascSortedList.RemoveAt(ascSortedList.Count - 1);
+                result += ascSortedList[ascSortedList.Count - 1];
+                ascSortedList.RemoveAt(ascSortedList.Count - 1);
+                ascSortedList.RemoveAt(0);
+            }
+            return result;
+        }
+    }
+}

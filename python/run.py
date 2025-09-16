f65d77d93eff22ea7de88af68fb901c5921d6bdb
Myo Set Paing <138693417+KraftReport@users.noreply.github.com>
2025-09-16 16:50:50 +0630

    fibonacci

--- python/run.py
+++ python/run.py
@@ -1,4 +1,7 @@
 from solutions.counting_bits import CountingBits
+from solutions.fibonacci import Fibonacci
 
 
-print(CountingBits().solution(5))
+# print(CountingBits().solution(5))
+
+print(Fibonacci().solution(3))

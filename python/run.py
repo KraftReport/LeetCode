59566c021b43b774567ecb72014c973291cc402a
Myo Set Paing <138693417+KraftReport@users.noreply.github.com>
2025-09-23 23:21:55 +0630

    two sneaky numbers of digitville

--- python/run.py
+++ python/run.py
@@ -1,10 +1,13 @@
 from solutions.counting_bits import CountingBits
 from solutions.fibonacci import Fibonacci
 from solutions.number_of_good_pairs import NumberOfGoodPairs
+from solutions.two_sneaky_numbers_of_digitville import TwoSneakyNumbersOfDigitVille
 
 
 # print(CountingBits().solution(5))
 
 # print(Fibonacci().solution(3))
 
-print(NumberOfGoodPairs().solution([1,2,3,1,1,3]))
+# print(NumberOfGoodPairs().solution([1,2,3,1,1,3]))
+
+print(TwoSneakyNumbersOfDigitVille().solution([0,1,1,0]))

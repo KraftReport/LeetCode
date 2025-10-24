50f3eec9ee2f4cb925e95ed097e954cee7f97429
Myo Set Paing <138693417+KraftReport@users.noreply.github.com>
2025-10-24 22:39:19 +0630

    sum of unique elements

--- java/program/src/program/Main.java
+++ java/program/src/program/Main.java
@@ -4,12 +4,13 @@ import java.util.List;
 
 import program.code.MaxFrequencyElements;
 import program.code.SubarraysDistinctElementSumOfSquares;
+import program.code.SumOfUniqueElements;
 
 public class Main {
 
 	public static void main(String[] args) {
 //		System.out.println(MaxFrequencyElements.solution(new int[] {1,2,2,3,1,4}));
-		System.out.println(SubarraysDistinctElementSumOfSquares.solution(List.of(1,2,1)));
-		
+//		System.out.println(SubarraysDistinctElementSumOfSquares.solution(List.of(1,2,1)));
+		System.out.println(SumOfUniqueElements.solution(new int[] {1,2,3,2}));
 	}
 }
